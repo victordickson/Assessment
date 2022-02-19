0a0f752edf07257543cb1a1259c0a5065034e7cf
vick <victordickson474@gmail.com>
2022-02-19 23:46:38 +0100

    first commit

---
+++ microservices/src/frontend/server.js
@@ -0,0 +1,20 @@
+
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = process.env.PORT || 8080;
+
+//Serve website
+app.use(express.static(path.join(__dirname, "public")));
+
+//Client side routing fix on page refresh or direct browsing to non-root directory
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"), err => {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
+
+//Start the server
+app.listen(port, () => console.log(`Frontend microservice listening on port ${port}!`));
